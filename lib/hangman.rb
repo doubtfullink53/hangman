c4b10783014263b0807fe878bd543d59fb52f3e1
Robert <doubtfullink53@gmail.com>
2022-11-04 16:24:16 -0400

    working version with terminal images , added drawille gem and chunky_png

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,52 +1,95 @@
+require 'drawille'
+require 'chunky_png'
+
+include ChunkyPNG
+
 class Main
+  def initialize
+    @number = 0
+  end
+
   def secret_word
     words = File.read('google-10000-english-no-swears.txt').split
-    puts @word = words.select { |w| w.size > 5 && w.size < 12 }.sample
+     @word = words.select { |w| w.size > 5 && w.size < 12 }.sample # secret word 
   end
 
   def draw_board
-    puts '  --------'
-    puts '  |      ' ' '
-    puts '  |'
-    puts '  |'
-    puts '  |'
-    puts '  |'
-    puts '  |'
-    puts '  |'
-    puts '--------'
+    canvas = Drawille::Canvas.new
+
+    def draw(canvas, img, xoffset = 0)
+      (0..img.dimension.width - 1).each do |x|
+        (0..img.dimension.height - 1).each do |y|
+          r = Color.r(img[x, y])
+          g = Color.g(img[x, y])
+          b = Color.b(img[x, y])
+          canvas.set(x + xoffset, y) if (r + b + g) > 100
+        end
+      end
+    end
+
+    draw canvas, Image.from_file("./images/tile00#{@number}.png")
+
+    puts canvas.frame
   end
 
   def guess
     puts 'Guess a letter.'
-    #  @guessed_letters = gets.chomp
-    @guess_array = Array.new
-    @guess_array << gets.chomp
-    p @guess_array
+    @guess = STDIN.gets.chomp
+    @guess_array += @guess.split
   end
 
   def valid_char
-    if @guessed_letters == '' || @guessed_letters.nil? || !@guessed_letters.match?(/[A-Za-z]/)
+    if !@guess.match?(/[A-Za-z]/)
       puts 'Entered an invalid character'
-      guess_again
+      puts @hidden_word
+      @number += 1
+      draw_board
+    elsif @word.include?(@guess)
+      puts @hidden_word = @word.chars.map { |c| @guess_array.include?(c) ? c : ' _ ' }.join
+    end
+    guess_again
+  end
 
-    else
-      # @indexes = @word.chars.each_with_index.select { |x, _i| x == @letter }.map(&:last)
+  def end_of_game
+    if @number == 6
+      @number = 0
+      puts 'You lost!'
+      puts 'Do you want to play again YES (N) or NO (N)?'
+      playagain = STDIN.gets.chomp
 
-      # puts @hidden_word = @word.chars.map { |c| @guessed_letters.include?(c) ? c : ' _ ' }.join
+      if playagain.downcase == 'y' || playagain.downcase == 'yes'
+        start
+      else
+        exit
+      end
+    end
 
-      guess_again
+    if @hidden_word == @word
+      @number = 0
+      puts 'You Won!!!!'
+      puts 'Do you want to play again YES (N) or NO (N)?'
+      playagain = STDIN.gets.chomp
+
+      if playagain.downcase == 'y' || playagain.downcase == 'yes'
+        start
+      else
+        exit
+      end
     end
   end
 
   def guess_again
-    draw_board
+    # draw_board
+    end_of_game
     guess
     valid_char
   end
 
   def start
-    secret_word
     draw_board
+    @guess_array = []
+    @number_of_guesses = 0
+    secret_word
     guess
     valid_char
   end
